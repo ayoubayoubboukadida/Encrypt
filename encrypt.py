855906b5266de0953975bd63defe89baff94c4d1
ayoubayoubboukadida <86422631+ayoubayoubboukadida@users.noreply.github.com>
2022-01-05 10:07:09 +0100

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,1 +1,22 @@
-print("ayoub")
+print ("<---------------------------------------------------------------->")
+print ("1) Encrypt")
+print ("2) decrypt")
+print ("<---------------------------------------------------------------->")
+print("")
+
+
+
+a =input("Choose a number -->")
+if a =="1":
+    def encryption_text(text):
+        x="abcdefghijklmnopqrstuvwxyz "
+        y="12345678900987654321012345"
+        enc_text=""
+        for char in text :
+            index=x.index(char)
+            enc_text+=y[index]
+        return enc_text
+    text=encryption_text(input("Type something for encryption -->"))
+    print("Encryption has been made:  "+text)
+else:
+    print("No")
